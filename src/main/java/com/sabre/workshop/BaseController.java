4970116b758408d990fe0cc830548eca01e5b6d2
Marcin <marcin.zajda@gmail.com>
2016-02-20 10:39:33 +0100

    Hello World in http server

--- src/main/java/com/sabre/workshop/BaseController.java
+++ src/main/java/com/sabre/workshop/BaseController.java
@@ -4,8 +4,20 @@ package com.sabre.workshop;
  * Created by Marcin on 2016-02-20.
  */
 
-import org.springframework.boot.autconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
+@Controller
+@EnableAutoConfiguration
 public class BaseController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hellow World!";
+    }
+
 }
--- src/main/java/com/sabre/workshop/SpringTrainingApp.java
+++ src/main/java/com/sabre/workshop/SpringTrainingApp.java
@@ -1,12 +1,16 @@
 package com.sabre.workshop;
 
+import org.springframework.boot.SpringApplication;
+
 /**
  * Created by Marcin on 2016-02-20.
  */
 public class SpringTrainingApp {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        SpringApplication.run(BaseController.class, args);
     }
 
 }
