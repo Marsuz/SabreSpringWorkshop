68de87481d3e3457ea3b1f1de417f9b8bc245f90
Marcin <marcin.zajda@gmail.com>
2016-02-20 09:36:37 +0100

    Hello World with Gradle

---
+++ src/main/java/com/sabre/workshop/SpringTrainingApp.java
@@ -0,0 +1,12 @@
+package com.sabre.workshop;
+
+/**
+ * Created by Marcin on 2016-02-20.
+ */
+public class SpringTrainingApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
