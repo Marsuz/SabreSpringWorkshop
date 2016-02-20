4003eca71f344944b7f080a5ae2602c2fc5eb44b
Marcin <marcin.zajda@gmail.com>
2016-02-20 10:34:16 +0100

    Base Controller

---
+++ src/main/java/com/sabre/workshop/BaseController.java
@@ -0,0 +1,11 @@
+package com.sabre.workshop;
+
+/**
+ * Created by Marcin on 2016-02-20.
+ */
+
+import org.springframework.boot.autconfigure.EnableAutoConfiguration;
+
+
+public class BaseController {
+}
